88765ae4d8c5c42e9c3a839e1516522b8095cc97
fahad <fahadi.alichioni@gmail.com>
2025-09-19 14:41:14 +0200

    fix remaining code quality issues

--- backend/bad_code.py
+++ backend/bad_code.py
@@ -1,16 +1,9 @@
 # Fichier de test avec des erreurs volontaires
-from django.http import JsonResponse
 
 
 def bad_function():
     x = 1 + 2 + 3
     y = "hello" + "world"
-
-    # Ligne trop longue qui dépasse 88 caractères et qui va faire échouer flake8
-    very_long_string = (
-        "Cette ligne est beaucoup trop longue et va dépasser la limite de "
-        "88 caractères imposée par flake8 et black"
-    )
 
     return x, y
 
