a9d911c710691dacd6541256c158cd442c737b5e
ziouane-rayane <ziouanerayane9@gmail.com>
2025-11-05 22:20:52 +0100

    auth jwt

--- backend/serializers/__init__.py
+++ backend/serializers/__init__.py
@@ -1,1 +1,16 @@
 # Serializers package for Django REST Framework
+from .auth_serializer import (
+    AuthResponseSerializer,
+    LoginSerializer,
+    RegisterSerializer,
+    UserSerializer,
+)
+from .user_serializer import UserSerializer as UserModelSerializer
+
+__all__ = [
+    "RegisterSerializer",
+    "LoginSerializer",
+    "UserSerializer",
+    "AuthResponseSerializer",
+    "UserModelSerializer",
+]
