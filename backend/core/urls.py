7df564d6c2bdaacd5abfbbb1ee7befba670f46e8
fahad <fahadi.alichioni@gmail.com>
2025-11-12 09:58:10 +0100

    fix merge issue

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('apps.auth.urls')),
     path('api/users/', include('apps.users.urls')),
-    
 ]
