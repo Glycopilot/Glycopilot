e695702914b56015f34df8dc4627afa72901984e
Lys Yoyo <lysmambouana3@gmail.com>
2025-11-27 01:03:39 +0100

    feat:reset password with

---
+++ backend/apps/auth/signals.py
@@ -0,0 +1,20 @@
+from django.dispatch import receiver
+from django_rest_passwordreset.signals import reset_password_token_created
+from apps.auth.email_smtp import send_reset_password_email
+from django.conf import settings
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    """
+    Signal déclenché automatiquement lorsque quelqu'un demande une réinitialisation de mot de passe.
+    Envoie un email avec un lien contenant le token.
+    """
+    user_email = reset_password_token.user.email
+    token = reset_password_token.key
+
+    # Construire le lien complet pour réinitialiser le mot de passe
+    # Remplace 'FRONTEND_URL' par l'URL de ton frontend ou endpoint de reset
+    reset_link = f"{settings.FRONTEND_URL}/reset-password?token={token}"
+
+    # Envoi de l'email
+    send_reset_password_email(user_email, reset_link)
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/password_reset/',  include("django_rest_passwordreset.urls", namespace="password_reset")),
     path("api/auth/", include("apps.auth.urls")),
     path("api/users/", include("apps.users.urls")),
 ]
