e96f1de69179013c1cfa73381d2e1c8751d25570
hsalemh <salem.hamiane@outlook.fr>
2025-12-05 10:40:08 +0100

    fix: lint commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,6 +19,6 @@ module.exports = {
     'subject-case': [2, 'never', ['upper-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 250],
   },
 };
