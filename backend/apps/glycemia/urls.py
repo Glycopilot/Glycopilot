41c80afd32d15c44ebf8be7a8e4289b1c601241b
hsalemh <105912631+hsalemh@users.noreply.github.com>
2025-12-22 17:43:29 +0100

    Chore/setup venv automation (#36)
    * chore: update venv and branch name
    
    * chore: update pre commit to bypass flake
    
    * fix: isort and balck
    
    * style: apply isort and black formatting
    
    * feat: add requirement to setup pipeline prob
    
    * fix: remove duplicate migrations causing duplicate column error

--- backend/apps/glycemia/urls.py
+++ backend/apps/glycemia/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path, include
+from django.urls import include, path
+
 from rest_framework.routers import DefaultRouter
+
 from .views import GlycemiaViewSet
 
 router = DefaultRouter()
-router.register(r'', GlycemiaViewSet, basename='glycemia')
+router.register(r"", GlycemiaViewSet, basename="glycemia")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
