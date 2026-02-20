b042ce11e55e5acd1b7f2b350acf42794ba4bf17
hsalemh <105912631+hsalemh@users.noreply.github.com>
2026-02-20 15:03:06 +0100

    Feat/deploy bucket cloud (#121)
    * feat: add architecture for infra
    
    * feat: set up for requirement for our aws env
    
    * feat: add a terraform plan

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "la region aws pour les déploiement"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "instance_type" {
+  description = "Le type d'instance EC2"
+  type        = string
+  default     = "t3.micro"
+}
