9ecc2640673988662b02bd0a798a3e12b77d3c81
hsalemh <salem.hamiane@outlook.fr>
2026-02-22 05:56:43 +0100

    feat: deploy the infra onto aws

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -86,3 +86,135 @@ resource "aws_security_group" "web_sg" {
     Name = "glycopilot-web-sg"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "glycopilot-key-deployer"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public_subnet.id
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  key_name = aws_key_pair.deployer.key_name
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = <<-EOF
+                #!/bin/bash
+                curl -fsSL https://get.docker.com -o get-docker.sh
+                sh get-docker.sh
+
+                usermod -aG docker ubuntu
+
+                systemctl enable docker
+                systemctl start docker
+                
+                EOF
+
+  user_data_replace_on_change = true
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "glycopilot-ec2-plan-a"
+  }
+}
+
+resource "aws_eip" "web_eip" {
+  instance = aws_instance.web.id
+  domain   = "vpc"
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "glycopilot-eip"
+  }
+}
+
+resource "aws_s3_bucket" "media" {
+  bucket = "glycopilot-aws-s3-bucket-img-artifacts"
+
+  tags = {
+    Name = "glycopilot-media"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "media_versioning" {
+  bucket = aws_s3_bucket.media.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "glycopilot-ec2-s3-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "glycopilot-s3-access-policy"
+  description = "Autorise l'EC2 à lire écrire dans le bucket media"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.media.arn,
+          "${aws_s3_bucket.media.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_s3_attach" {
+  role       = aws_iam_role.ec2_s3_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "glycopilot-ec2-profile"
+  role = aws_iam_role.ec2_s3_role.name
+}
